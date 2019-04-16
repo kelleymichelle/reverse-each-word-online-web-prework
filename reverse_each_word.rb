0fbee036445a328d7d8b9bf8a48d7db05fd9b066
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-16 03:40:23 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+def reverse_each_word(sent)
+  arr = sent.split
+  #new_arr = [ ]
+   # arr.each do |word|
+    #  new_arr << word.reverse
+   #end
+    #newsent = new_arr.join(" ")
+     # return newsent
+  
+  arr.collect do |word| 
+    word.reverse
+      return arr.join(" ")
+
+  end
+end  
